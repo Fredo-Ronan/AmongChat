f318219e97438c628a9236451e5f2fa82e88c920
Fredo-Ronan <fredocode06@gmail.com>
2025-09-09 19:42:07 +0700

    gradle version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "0.0.1-alpha.1"
+        versionName = "0.0.2-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
